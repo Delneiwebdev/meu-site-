f7b8263998e4f88a257459551f74771d2b4b82a4
Delnei de Sá <delnei@proton.me>
2024-03-03 14:41:04 -0300

    aula de Js

---
+++ Aula de JS/index.js
@@ -0,0 +1,3 @@
+alert("Erro")//tipo de saida 1
+document.getElementById ("paragrafo").innerHTML= "Olá, Mundo!";  //Mostra a mensagem Olá, Mundo no elemento HTML com o id texto tipode saida 2
+console.log("ola tudo bem ?"); //tipo de saida 4
